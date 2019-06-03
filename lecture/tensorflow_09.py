6a6dd55c944c87d957ca5a41baf8453375085b04
SongHiiiii <juk08843@gmail.com>
2019-06-03 16:11:44 +0900

    lecture

--- lecture/tensorflow_09.py
+++ lecture/tensorflow_09.py
@@ -1,11 +1,14 @@
 import tensorflow as tf
 
+# word2vec에서 사용
+# 메모리를 효율적으로 사용 가능
 st = tf.SparseTensor(indices=[[0, 0], [1, 2]], values=[1.0, 1.0], dense_shape=[3, 4])
 
 dense = tf.sparse_tensor_to_dense(st)
 
 c = tf.constant([[1.0, 2.0, 3.0, 4.0]], dtype=tf.float32)
 print(c.shape)
+# 행과 열을 바꿔주는 과정
 tp = tf.transpose(c)
 print(tp.shape)
 dense_matmul = tf.sparse_tensor_dense_matmul(st, tp)
