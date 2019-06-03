dcf77bf7d22a564f31cd31af57407d606ae042a5
SongHiiiii <juk08843@gmail.com>
2019-06-03 14:46:24 +0900

    lecture

--- lecture/tensorflow_05.py
+++ lecture/tensorflow_05.py
@@ -4,6 +4,9 @@ x = tf.placeholder("int32")
 y = tf.placeholder("int32")
 
 z = tf.multiply(x, y)
+# 행렬 곱셈은 matmul
+# 같은 위치 곱셈은 element-wise
+# 같은 행의 곱셈은 multiply
 
 init = tf.global_variables_initializer()
 with tf.Session() as sess:
